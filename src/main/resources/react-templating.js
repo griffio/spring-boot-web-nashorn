d7635174cb079b3e63cd0e47b58a184b1ef43455
griffio <griffio@users.noreply.github.com>
2015-04-08 17:45:12 +0100

    renderToStaticMarkup

--- src/main/resources/react-templating.js
+++ src/main/resources/react-templating.js
@@ -9,8 +9,8 @@ function render(template, model) {
     var data = {};
     for(var k in model) data[k]=model[k];
     var element = React.createElement(eval(template), data);
-    // renderToStaticMarkup removes any react markup attributes (e.g data-reactid=".ho906m2sqd.1.0.0.3") from rendered markup
-    return React.renderToString(element);
+    // renderToStaticMarkup removes any react markup attributes (e.g data-reactid=".ho906m2sqd.1.0.0.3" from rendered markup
+    return React.renderToStaticMarkup(element);
 }
 
 function renderJsx(template, model) {
